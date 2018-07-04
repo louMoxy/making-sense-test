117300d1cb55424bb532c850880ce0a1d6dc939c
louMoxy <louisemoxy@gmail.com>
2018-07-04 22:53:19 +0100

    Use the numberMapping object to create the if statement
    - Keep to using the for loop to improve readability.
    - Revert if the if statements to === 0 rather than check for falsey to improve readability
    - Pass the numberMapping object to funciton so the returned logged string can be easily changed
    - Use the numberMapping object to create the for loop as the logic is the same

--- making-sense.js
+++ making-sense.js
@@ -1,35 +1,25 @@
-const numberMapping = {
+const makingSenseMap = {
   3: "Making",
   5: "Sense"
 };
 
-function numberLogLoop(startNum = 1, endNum = 100) {
+const taDaMap = {
+  3: "Ta",
+  5: "Da"
+};
+
+function numberLogLoop(numberMapping, startNum = 1, endNum = 100) {
   for (let currentNum = startNum; currentNum <= endNum; currentNum++) {
     let returnString = "";
-    if (!(currentNum % 3)) {
-      returnString += numberMapping[3];
-    }
-    if (!(currentNum % 5)) {
-      returnString += returnString.length > 0 ? ` ${numberMapping[5]}` : numberMapping[5];
+    for (numberKey in numberMapping) {
+      if (currentNum % numberKey === 0) {
+        returnString +=
+          returnString.length > 0 ? ` ${numberMapping[numberKey]}` : numberMapping[numberKey];
+      }
     }
     console.log(returnString || currentNum);
   }
 }
 
-// numberLogLoop();
-
-function numberRecursion(currentNum = 1, max = 100) {
-  let returnString = "";
-  if (!(currentNum % 3)) {
-    returnString += numberMapping[3];
-  }
-  if (!(currentNum % 5)) {
-    returnString += returnString.length > 0 ? ` ${numberMapping[5]}` : numberMapping[5];
-  }
-  console.log(returnString || currentNum);
-  if (currentNum < max) {
-    numberRecursion(++currentNum, max);
-  }
-}
-
-numberRecursion();
+numberLogLoop(makingSenseMap);
+numberLogLoop(taDaMap);
